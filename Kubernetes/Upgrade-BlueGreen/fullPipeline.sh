21289d302363c4cf1a66cb5ce5c0df7bc699dd53
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2021-02-13 16:08:38 +0200

    Add blue/green

--- Kubernetes/Upgrade-BlueGreen/fullPipeline.sh
+++ Kubernetes/Upgrade-BlueGreen/fullPipeline.sh
@@ -4,6 +4,7 @@ date
 echo "Clean All"
 kubectl -n dizertatie-minikube-24349559-canary delete all --all
 kubectl -n dizertatie-minikube-24349559-canary delete ingress --all
+kubectl -n dizertatie-minikube-24349559-canary delete IngressRoute --all
 echo "Done cleaning"
 sleep 60
 date
@@ -11,7 +12,7 @@ echo "Start version 1"
 kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version1.yml -f service-version1.yml -f ingress-version1.yml
 sleep 300
 date
-echo "Start canary"
+echo "Start version 2"
 kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version2.yml -f service-version2.yml -f ingress-version2.yml
 sleep 300
 date
@@ -20,4 +21,6 @@ kubectl -n dizertatie-minikube-24349559-canary delete -f deployment-version2.yml
 sleep 300
 date
 echo "Delete all"
-kubectl -n dizertatie-minikube-24349559-canary delete -f deployment-version1.yml -f service-version1.yml -f ingress-version1.yml
+kubectl -n dizertatie-minikube-24349559-canary delete all --all
+kubectl -n dizertatie-minikube-24349559-canary delete ingress --all
+kubectl -n dizertatie-minikube-24349559-canary delete IngressRoute --all
