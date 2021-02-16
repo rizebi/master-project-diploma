33dbb8603a8a3326efb2b60bb8dd7a1bd75e9232
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2021-02-16 23:13:15 +0200

    Continue pipelines

--- Kubernetes/Upgrade-BlueGreen/fullPipeline.sh
+++ Kubernetes/Upgrade-BlueGreen/fullPipeline.sh
@@ -1,26 +1,48 @@
 # /bin/bash
 
-date
-echo "Clean All"
-kubectl -n dizertatie-minikube-24349559-canary delete all --all
-kubectl -n dizertatie-minikube-24349559-canary delete ingress --all
-kubectl -n dizertatie-minikube-24349559-canary delete IngressRoute --all
-echo "Done cleaning"
-sleep 60
-date
-echo "Start version 1"
-kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version1.yml -f service-version1.yml -f ingress-version1.yml
-sleep 300
-date
-echo "Start version 2"
-kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version2.yml -f service-version2.yml -f ingress-version2.yml
-sleep 300
-date
-echo "Rollback to only version 1"
-kubectl -n dizertatie-minikube-24349559-canary delete -f deployment-version2.yml -f service-version2.yml -f ingress-version2.yml
-sleep 300
-date
-echo "Delete all"
-kubectl -n dizertatie-minikube-24349559-canary delete all --all
-kubectl -n dizertatie-minikube-24349559-canary delete ingress --all
-kubectl -n dizertatie-minikube-24349559-canary delete IngressRoute --all
+if [[ "x$1" == "xfull" || "x$1" == "xdelete" || "x$1" == "xv1" ]]; then
+  date
+  echo "Clean All"
+  kubectl delete all --all
+  kubectl delete ingress --all
+  kubectl delete IngressRoute --all
+  echo "Done cleaning"
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 60
+  fi
+fi
+
+if [[ "x$1" == "xfull" || "x$1" == "xv1" ]]; then
+  date
+  echo "Start version 1"
+  kubectl apply -f deployment-version1.yml -f service-version1.yml -f ingress-version1.yml
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 300
+  fi
+fi
+
+if [[ "x$1" == "xfull" || "x$1" == "xv2" ]]; then
+  date
+  echo "Start version 2"
+  kubectl apply -f deployment-version2.yml -f service-version2.yml -f ingress-version2.yml
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 300
+  fi
+fi
+
+if [[ "x$1" == "xfull" || "x$1" == "xrollback" ]]; then
+  date
+  echo "Rollback to only version 1"
+  kubectl delete -f deployment-version2.yml -f service-version2.yml -f ingress-version2.yml
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 300
+  fi
+fi
+
+if [[ "x$1" == "xfull"]]; then
+  date
+  echo "Delete all"
+  kubectl delete all --all
+  kubectl delete ingress --all
+  kubectl delete IngressRoute --all
+fi
--- Kubernetes/Upgrade-Recreate/fullPipeline.sh
+++ Kubernetes/Upgrade-Recreate/fullPipeline.sh
@@ -1,28 +1,50 @@
 # /bin/bash
 
-date
-echo "Clean All"
-kubectl -n dizertatie-minikube-24349559-canary delete all --all
-kubectl -n dizertatie-minikube-24349559-canary delete ingress --all
-kubectl -n dizertatie-minikube-24349559-canary delete IngressRoute --all
-echo "Done cleaning"
-sleep 60
-date
-echo "Start version 1"
-kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version1.yml -f service-version1.yml -f ingress.yml
-sleep 300
-date
-echo "Stop version 1 and start version 2"
-kubectl -n dizertatie-minikube-24349559-canary delete -f deployment-version1.yml -f service-version1.yml
-kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version2.yml -f service-version2.yml
-sleep 300
-date
-echo "Rollback to version 1"
-kubectl -n dizertatie-minikube-24349559-canary delete -f deployment-version2.yml -f service-version2.yml
-kubectl -n dizertatie-minikube-24349559-canary apply -f deployment-version1.yml -f service-version1.yml
-sleep 300
-date
-echo "Delete all"
-kubectl -n dizertatie-minikube-24349559-canary delete all --all
-kubectl -n dizertatie-minikube-24349559-canary delete ingress --all
-kubectl -n dizertatie-minikube-24349559-canary delete IngressRoute --all
+if [[ "x$1" == "xfull" || "x$1" == "xdelete" || "x$1" == "xv1" ]]; then
+  date
+  echo "Clean All"
+  kubectl delete all --all
+  kubectl delete ingress --all
+  kubectl delete IngressRoute --all
+  echo "Done cleaning"
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 60
+  fi
+fi
+
+if [[ "x$1" == "xfull" || "x$1" == "xv1" ]]; then
+  date
+  echo "Start version 1"
+  kubectl apply -f deployment-version1.yml -f service-version1.yml -f ingress.yml
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 300
+  fi
+fi
+
+if [[ "x$1" == "xfull" || "x$1" == "xv2" ]]; then
+  date
+  echo "Stop version 1 and start version 2"
+  kubectl delete -f deployment-version1.yml -f service-version1.yml
+  kubectl apply -f deployment-version2.yml -f service-version2.yml
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 300
+  fi
+fi
+
+if [[ "x$1" == "xfull" || "x$1" == "xrollback" ]]; then
+  date
+  echo "Rollback to version 1"
+  kubectl delete -f deployment-version2.yml -f service-version2.yml
+  kubectl apply -f deployment-version1.yml -f service-version1.yml
+  if [[ "x$1" == "xfull" ]]; then
+    sleep 300
+  fi
+fi
+
+if [[ "x$1" == "xfull"]]; then
+  date
+  echo "Delete all"
+  kubectl delete all --all
+  kubectl delete ingress --all
+  kubectl delete IngressRoute --all
+fi
